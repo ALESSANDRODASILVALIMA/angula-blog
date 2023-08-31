45111cc7f6b244cce2bb8a0f92d382de731757f9
ALESSANDRODASILVALIMA <alessandrodasilva93@gmail.com>
2023-08-31 16:48:57 -0300

    adicionada estrutura pages e modificações em componets

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css']
+})
+export class BigCardComponent {
+  data: Date = new Date();
+  constructor(){
+    console.log(this.data)
+  }
+}
